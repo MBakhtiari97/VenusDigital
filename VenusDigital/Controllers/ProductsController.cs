acc49715ff50b5c2048492c082b310003e703580
mohammadba86@yahoo.com <mohammadba86@yahoo.com>
2022-02-18 17:41:17 +0330

    Showing product Details

---
+++ VenusDigital/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VenusDigital.Data.Repositories;
+
+namespace VenusDigital.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductsRepository _productsRepository;
+
+        public ProductsController(IProductsRepository productsRepository)
+        {
+            _productsRepository = productsRepository;
+        }
+        public IActionResult ShowProductDetails()
+        {
+            //TODO:FILL THIS PART
+            return View();
+        }
+    }
+}
---
+++ VenusDigital/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace VenusDigital.Models.ViewModels
+{
+    public class ProductDetailsViewModel
+    {
+        public string MainImage { get; set; }
+        public string Title { get; set; }
+        public float Score { get; set; }
+        public int ReviewsCount { get; set; }
+        public string Availability { get; set; }
+        public string ShortDescription { get; set; }
+        public string FullDescription { get; set; }
+        public decimal MainPrice { get; set; }
+        public decimal SalePrice { get; set; }
+    }
+}
