d0edae35116ff87aa037cc3cff047e63842f58bf
mohammadba86@yahoo.com <mohammadba86@yahoo.com>
2022-02-21 07:57:27 +0330

    Completed Showing Categories

---
+++ VenusDigital/ViewComponents/LeftSideCategoriesComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VenusDigital.Data.Repositories;
+
+namespace VenusDigital.ViewComponents
+{
+    public class LeftSideCategoriesComponent:ViewComponent
+    {
+        private ICategoryRepository _categoryRepository;
+
+        public LeftSideCategoriesComponent(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("/Views/ViewComponents/LeftSideCategoriesComponent.cshtml", _categoryRepository.GetCategories());
+
+        }
+    }
+}
