b28257d6804469b96c2b5aafa56571f2a3df9d48
mohammadba86@yahoo.com <mohammadba86@yahoo.com>
2022-02-25 17:25:37 +0330

    Completed Compare Section

--- VenusDigital/Controllers/CompareController.cs
+++ VenusDigital/Controllers/CompareController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VenusDigital.Data.Repositories;
 
 namespace VenusDigital.Controllers
 {
+    [Authorize]
     public class CompareController : Controller
     {
+        #region InjectRepository
+
+        private ICompareRepository _compareRepository;
+        public INotyfService _notyfServic { get; }
+
+        public CompareController(ICompareRepository compareRepository, INotyfService notyfService)
+        {
+            _compareRepository = compareRepository;
+            _notyfServic = notyfService;
+        }
+
+
+        #endregion
+        [Route("Compare")]
         public IActionResult ShowCompareList()
         {
-            return View();
+            int userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier).ToString());
+
+            return View(_compareRepository.GetCompareItems(userId));
         }
+
+        #region CompareOperations
+
+        public IActionResult AddToCompare(int productId)
+        {
+            int userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier).ToString());
+            _compareRepository.AddToCompare(productId, userId);
+            _notyfServic.Success("Item Successfully Added To Compare List!");
+            return RedirectToAction("ShowCompareList");
+        }
+
+        public IActionResult RemoveFromCompare(int compareId)
+        {
+            var compareItem = _compareRepository.getCompareById(compareId);
+            _compareRepository.RemoveFromCompare(compareItem);
+            _notyfServic.Success("Item Successfully Removed From Your Compare List!");
+            return RedirectToAction("ShowCompareList");
+        }
+
+        #endregion
+
     }
 }
--- VenusDigital/Data/Repositories/ICompareRepository.cs
+++ VenusDigital/Data/Repositories/ICompareRepository.cs
@@ -1,7 +1,17 @@
-﻿namespace VenusDigital.Data.Repositories
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using VenusDigital.Models;
+
+namespace VenusDigital.Data.Repositories
 {
     public interface ICompareRepository
     {
+        IEnumerable<Compare> GetCompareItems(int userId);
+        void AddToCompare(int productId, int userId);
+        void RemoveFromCompare(Compare compareItem);
+        Compare getCompareById(int compareId);
+
     }
 
     public class CompareRepository : ICompareRepository
@@ -13,5 +23,36 @@
             _context = context;
         }
 
+        public void AddToCompare(int productId,int userId)
+        {
+            Compare compareItem = new Compare()
+            {
+                ProductId = productId,
+                UserId = userId
+            };
+            _context.Compare.Add(compareItem);
+            _context.SaveChanges();
+        }
+
+        public Compare getCompareById(int compareId)
+        {
+            return _context.Compare
+                .Find(compareId);
+        }
+
+        public IEnumerable<Compare> GetCompareItems(int userId)
+        {
+            return _context.Compare
+                .Include(c=>c.Products)
+                .ThenInclude(c=>c.ProductGalleries)
+                .Where(c => c.UserId == userId)
+                .ToList();
+        }
+
+        public void RemoveFromCompare(Compare compareItem)
+        {
+            _context.Compare.Remove(compareItem);
+            _context.SaveChanges();
+        }
     }
 }
