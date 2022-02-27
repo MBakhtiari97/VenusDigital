5c2aee8944366eb0994b639e007a10e8f9ac98aa
mohammadba86@yahoo.com <mohammadba86@yahoo.com>
2022-02-27 19:51:28 +0330

    Completing Show Product Details In Admin Area

---
+++ VenusDigital/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VenusDigital.Data;
+using VenusDigital.Models;
+
+namespace VenusDigital.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly VenusDigitalContext _context;
+
+        public ProductsController(VenusDigitalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+        // GET: Admin/Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var products = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            ViewBag.image = _context.ProductGalleries
+                .Where(p => p.ProductId == id)
+                .Select(p => p.ImageName)
+                .First();
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        // GET: Admin/Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,ProductTitle,ProductShortDescription,ProductLongDescription,ProductInStock,ProductQuantityInStock,ProductMainPrice,GalleryId,ProductScore,TagId,ReviewId,ProductOnSalePrice,SalePercent,FeatureId,CreateDate")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                products.CreateDate = DateTime.Now;
+                _context.Add(products);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+
+        // GET: Admin/Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+            return View(products);
+        }
+
+        // POST: Admin/Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductTitle,ProductShortDescription,ProductLongDescription,ProductInStock,ProductQuantityInStock,ProductMainPrice,GalleryId,ProductScore,TagId,ReviewId,ProductOnSalePrice,SalePercent,FeatureId,CreateDate")] Products products)
+        {
+            if (id != products.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(products);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductsExists(products.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+
+        // GET: Admin/Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        // POST: Admin/Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+            _context.Products.Remove(products);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductsExists(int id)
+        {
+            return _context.Products.Any(e => e.ProductId == id);
+        }
+    }
+}
--- VenusDigital/Models/Products.cs
+++ VenusDigital/Models/Products.cs
@@ -10,29 +10,39 @@ namespace VenusDigital.Models
         public int ProductId { get; set; }
         [Required]
         [MaxLength(250)]
+        [Display(Name = "Title")]
         public string ProductTitle { get; set; }
+        [Display(Name = "Introduce")]
         [Required]
         [MaxLength(1500)]
         public string ProductShortDescription { get; set; }
+        [Display(Name = "Description")]
         [Required]
         public string ProductLongDescription { get; set; }
         [Required]
         [MaxLength(150)]
+        [Display(Name = "Status")]
         public string ProductInStock { get; set; }
         [Required]
+        [Display(Name = "Qunatity")]
         public int ProductQuantityInStock { get; set; }
         [Required]
+        [Display(Name = "Price($)")]
         public decimal ProductMainPrice { get; set; }
         [Required]
         public int GalleryId { get; set; }
         [Required]
+        [Display(Name = "Score")]
         public float ProductScore { get; set; }
         public int TagId { get; set; }
         public int ReviewId { get; set; }
+        [Display(Name = "Sale Price($)")]
         public decimal ProductOnSalePrice { get; set; }
+        [Display(Name = "Sale Percent")]
         public int SalePercent { get; set; }
         [Required]
         public int FeatureId { get; set; }
+        [Display(Name = "Create Date")]
         public DateTime CreateDate { get; set; }
 
         //Nav
