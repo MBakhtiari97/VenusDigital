770d50bbcff220d9964f7b97723806a8ae462fde
mohammadba86@yahoo.com <mohammadba86@yahoo.com>
2022-03-01 12:58:21 +0330

    Complete Manage Category Section

--- VenusDigital/Areas/Admin/Controllers/CategoriesController.cs
+++ VenusDigital/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using VenusDigital.Areas.Admin.Models;
 using VenusDigital.Data;
 using VenusDigital.Models;
 
@@ -19,6 +21,10 @@ namespace VenusDigital.Areas.Admin.Controllers
         {
             _context = context;
         }
+        [BindProperty] 
+        public CategoriesViewModel Category { get; set; }
+
+        #region CategoryIndex
 
         // GET: Admin/Categories
         public async Task<IActionResult> Index()
@@ -27,6 +33,10 @@ namespace VenusDigital.Areas.Admin.Controllers
             return View(await venusDigitalContext.ToListAsync());
         }
 
+
+        #endregion
+
+        #region CategoryDetails
         // GET: Admin/Categories/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -46,6 +56,10 @@ namespace VenusDigital.Areas.Admin.Controllers
             return View(categories);
         }
 
+        #endregion
+        
+        #region CreateCategory
+
         // GET: Admin/Categories/Create
         public IActionResult Create()
         {
@@ -62,6 +76,79 @@ namespace VenusDigital.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                //Getting name from bind property
+                if (Category.ParentCategoryBanner?.Length > 0)
+                    categories.ParentCategoryBanner = categories.CategoryId
+                                                      + Path.GetExtension(Category.ParentCategoryBanner.FileName);
+
+                if (Category.CategoryBanner?.Length > 0)
+                    categories.CategoryBanner = categories.CategoryId
+                                                + Path.GetExtension(Category.CategoryBanner.FileName);
+
+                //Saving insert values on database
+                _context.Add(categories);
+                await _context.SaveChangesAsync();
+
+                //checking for images and inserting it on server
+                if (Category.ParentCategoryBanner?.Length > 0)
+                {
+                    string filePathBanner = Path.Combine(
+                        Directory.GetCurrentDirectory(),
+                        "wwwroot",
+                        "Images",
+                        "categoriesBanner",
+                        categories.CategoryId + Path.GetExtension(Category.ParentCategoryBanner.FileName)
+                    );
+                    using (var stream = new FileStream(filePathBanner, FileMode.Create))
+                    {
+                        Category.ParentCategoryBanner.CopyTo(stream);
+                    }
+                }
+                if (Category.CategoryBanner?.Length > 0)
+                {
+                    string filePathBigBanner = Path.Combine(
+                        Directory.GetCurrentDirectory(),
+                        "wwwroot",
+                        "Images",
+                        "categoriesBigBanner",
+                        categories.CategoryId + Path.GetExtension(Category.CategoryBanner.FileName)
+                    );
+                    using (var stream = new FileStream(filePathBigBanner, FileMode.Create))
+                    {
+                        Category.CategoryBanner.CopyTo(stream);
+                    }
+                }
+                //redirecting after completing
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryName", categories.CategoryId);
+            return View(categories);
+        }
+
+        #endregion
+
+        #region CreateSubCategory
+
+        // GET: Admin/Categories/Create
+        public IActionResult CreateSubCategory()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryName");
+            ViewBag.ParentGroups = _context.Categories
+                .Where(c => c.ParentId == null)
+                .ToList();
+
+            return View();
+        }
+
+        // POST: Admin/Categories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateSubCategory([Bind("CategoryId,CategoryName,ParentId,ParentCategoryBanner,CategoryBanner")] Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
                 _context.Add(categories);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -70,6 +157,12 @@ namespace VenusDigital.Areas.Admin.Controllers
             return View(categories);
         }
 
+
+        #endregion
+
+        #region UpdateCategory
+
+       
         // GET: Admin/Categories/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -79,6 +172,9 @@ namespace VenusDigital.Areas.Admin.Controllers
             }
 
             var categories = await _context.Categories.FindAsync(id);
+            ViewBag.ParentGroups = _context.Categories
+                .Where(c => c.ParentId == null)
+                .ToList();
             if (categories == null)
             {
                 return NotFound();
@@ -105,6 +201,36 @@ namespace VenusDigital.Areas.Admin.Controllers
                 {
                     _context.Update(categories);
                     await _context.SaveChangesAsync();
+
+                    //checking for images and inserting it on server
+                    if (Category.ParentCategoryBanner?.Length > 0)
+                    {
+                        string filePathBanner = Path.Combine(
+                            Directory.GetCurrentDirectory(),
+                            "wwwroot",
+                            "Images",
+                            "categoriesBanner",
+                            categories.CategoryId + Path.GetExtension(Category.ParentCategoryBanner.FileName)
+                        );
+                        using (var stream = new FileStream(filePathBanner, FileMode.Create))
+                        {
+                            Category.ParentCategoryBanner.CopyTo(stream);
+                        }
+                    }
+                    if (Category.CategoryBanner?.Length > 0)
+                    {
+                        string filePathBigBanner = Path.Combine(
+                            Directory.GetCurrentDirectory(),
+                            "wwwroot",
+                            "Images",
+                            "categoriesBigBanner",
+                            categories.CategoryId + Path.GetExtension(Category.CategoryBanner.FileName)
+                        );
+                        using (var stream = new FileStream(filePathBigBanner, FileMode.Create))
+                        {
+                            Category.CategoryBanner.CopyTo(stream);
+                        }
+                    }
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -123,6 +249,10 @@ namespace VenusDigital.Areas.Admin.Controllers
             return View(categories);
         }
 
+        #endregion
+
+        #region DeleteCategory
+
         // GET: Admin/Categories/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -134,6 +264,7 @@ namespace VenusDigital.Areas.Admin.Controllers
             var categories = await _context.Categories
                 .Include(c => c.Category)
                 .FirstOrDefaultAsync(m => m.CategoryId == id);
+
             if (categories == null)
             {
                 return NotFound();
@@ -148,10 +279,21 @@ namespace VenusDigital.Areas.Admin.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var categories = await _context.Categories.FindAsync(id);
+            if (categories.ParentId == null)
+            {
+                _context.Categories
+                    .Where(c => c.ParentId == id)
+                    .ToList()
+                    .ForEach(c => _context.Categories.Remove(c)
+                    );
+            }
             _context.Categories.Remove(categories);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+
+        #endregion
 
         private bool CategoriesExists(int id)
         {
---
+++ VenusDigital/Areas/Admin/Models/CategoriesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace VenusDigital.Areas.Admin.Models
+{
+    public class CategoriesViewModel
+    {
+        public int CategoryId { get; set; }
+        [Display(Name = "Name")]
+        public string CategoryName { get; set; }
+        public Nullable<int> ParentId { get; set; }
+        public IFormFile ParentCategoryBanner { get; set; }
+        public IFormFile CategoryBanner { get; set; }
+    }
+}
