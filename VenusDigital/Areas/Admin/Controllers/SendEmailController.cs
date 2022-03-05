bb786371f709628f30c1bad4140c710cefa53223
mohammadba86@yahoo.com <mohammadba86@yahoo.com>
2022-03-05 09:50:54 +0330

    SendEmail Admin Module

---
+++ VenusDigital/Areas/Admin/Controllers/SendEmailController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEshop;
+using System.Threading.Tasks;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using VenusDigital.Areas.Admin.Models;
+using VenusDigital.Utilities;
+
+namespace VenusDigital.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class SendEmailController : Controller
+    {
+        #region Injection
+
+        private IViewRenderService _viewRenderService;
+        public INotyfService _notyfService;
+
+        public SendEmailController(IViewRenderService viewRenderService, INotyfService notyfService)
+        {
+            _viewRenderService = viewRenderService;
+            _notyfService = notyfService;
+        }
+
+        #endregion
+
+        #region SendCustomEmail
+
+        public IActionResult SendMail()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SendMail([Bind("Title,Description,Email")] SendEmailViewModel send)
+        {
+            if (!ModelState.IsValid)
+                return View(send);
+            //var body = await _viewRenderService.RenderToStringAsync("ManageEmails/_ActivateAccount";
+            SendEmail.Send(send.Email, send.Title, send.Description);
+            _notyfService.Success("Email Has Been Sent !");
+            return RedirectToAction("Index", "Home");
+        }
+
+        #endregion
+
+    }
+}
---
+++ VenusDigital/Areas/Admin/Models/SendEmailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VenusDigital.Areas.Admin.Models
+{
+    public class SendEmailViewModel
+    {
+        [Display(Name = "Email Title")]
+        public string Title { get; set; }
+        [Display(Name = "Email Description")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
